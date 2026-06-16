36f9280df643b7dc534f4fe80d17d99dd59fb8b2
Linh <55070319+linhvovo@users.noreply.github.com>
2026-06-16 16:39:25 +0800

    filled in the blanks in day1

--- day1.py
+++ day1.py
@@ -1,35 +1,35 @@
-import ...
+import torch
 
 # hyperparameters
-... = 4 # how many independent sequences will we process in parallel?
-... = 8 # what is the maximum context length for predictions?
-device = '...' ... torch.....is_available() ... '...'
+batch_size = 4 # how many independent sequences will we process in parallel?
+block_size = 8 # what is the maximum context length for predictions?
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # ------------
 
 torch.manual_seed(1337)
 
-with open('...', 'r', encoding='...') as f:
+with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # get all the unique characters that occur in this text
-chars = sorted(...(...(text)))
+chars = sorted(list(set(text)))
 alphabet_size = len(chars)
 # create a mapping from characters to integers
-... = { ch:i for i,ch in enumerate(chars) }
-... = { i:ch for i,ch in enumerate(chars) }
-... = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
-... = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
 # split data into training and validation data
-data = torch....(...(text), dtype=torch.long)
-n = ...(0.9*len(data)) # first 90% will be train, rest val
-... = data[:n]
-... = data[n:]
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9*len(data)) # first 90% will be train, rest val
+train_data = data[:n]
+val_data = data[n:]
 
 # generate a small batch of data of inputs x and targets y
-def ...(...):
-    data = ... ... split == 'train' ... ...
-    indices = torch....(len(data) - ..., (...,))
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    indices = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in indices])
     y = torch.stack([data[i+1:i+block_size+1] for i in indices])
     x, y = x.to(device), y.to(device)
@@ -38,16 +38,6 @@ def ...(...):
 # print out and check results
 xb, yb = get_batch('train')
 print('inputs:')
-print(xb.shape)
 print(xb)
 print('targets:')
-print(yb.shape)
 print(yb)
-
-print('----')
-
-for b in range(batch_size): # batch dimension
-    for t in range(block_size): # time dimension
-        context = xb[b, :t+1]
-        target = yb[b,t]
-        print(f"when input is {context.tolist()} the target: {target}")
